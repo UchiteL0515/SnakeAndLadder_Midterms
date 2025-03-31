650625df4ab2564403f5bbb106887a625b6698d5
UchiteLeiden <parantarlorenzoalec@gmail.com>
2025-03-31 14:49:40 +0800

    Error handled..

--- src/MainProgram.cs
+++ src/MainProgram.cs
@@ -3,18 +3,44 @@ using System.Collections.Generic;
 
 namespace SnakeNLadderGame{
 	
+	class Menu{
+
+	}
+
 	class MainProgram{
 		public static void Main(string[] args){
 			List<Player> Players = new List<Player>();
 
 			Console.Clear();
 			Console.WriteLine("Welcome to Snake and Ladders!");
-			Console.Write("Please enter number of players (2-4): ");
-			int numberOfPlayers = Convert.ToInt32(Console.ReadLine());
+			
+			int numberOfPlayers = 0;
+			bool validInput = false;
+
+			while(!validInput){
+				Console.Write("Please enter number of players (2-4): ");
+				string tempText = Console.ReadLine();
+
+				if(int.TryParse(tempText, out numberOfPlayers)){
+					if(numberOfPlayers >= 2 && numberOfPlayers <= 4){
+						validInput = true;
+					} else{
+						Console.WriteLine("Must be within the range...");
+					}
+				} else{
+					Console.WriteLine("Must be a valid number...");
+				}
+			}
 
 			for(int i = 1; i <= numberOfPlayers; i++){
 				Console.Write($"Enter player {i}'s name: ");
 				string name = Console.ReadLine();
+
+				while(string.IsNullOrWhiteSpace(name)){
+					Console.Write("Player name cannot be empty...try again: ");
+					name = Console.ReadLine();
+				}
+
 				Players.Add(new Player(name));
 			}
 
